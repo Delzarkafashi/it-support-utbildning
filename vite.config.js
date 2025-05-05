f0c579e275a09c9bb20981eb564bd444717541a7
Delzar Kafashi <delzarkafashi@email.com>
2025-05-05 13:13:09 +0200

    försöker hosta broweser

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/it-support-utbildning/',  // ←  GitHub Pages
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost:7266', // ← din backend-url
+        target: 'https://localhost:7266', // ← backend-url
         changeOrigin: true,
         secure: false
       }
