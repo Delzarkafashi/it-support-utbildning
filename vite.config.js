074601dbc853e6ae86ff0206270d7e1472c5db9e
Delzar Kafashi <delzarkafashi@email.com>
2025-05-05 13:40:18 +0200

    host broweser

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/it-support-utbildning/',  // ←  GitHub Pages
+export default defineConfig({  // ←  GitHub Pages
   plugins: [react()],
+  base: '/it-support-utbildning',
   server: {
     proxy: {
       '/api': {
